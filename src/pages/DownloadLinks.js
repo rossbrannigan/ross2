c38991e8fd95e5b15fd0eafa2b518d821e869c99
Ross Brannigan 2018 <rossbrannigan2018@Rosss-MacBook-Pro-2.local>
2024-06-29 02:55:14 +0700

    Updated CV

--- src/pages/DownloadLinks.js
+++ src/pages/DownloadLinks.js
@@ -3,18 +3,18 @@ import { FaFilePdf, FaFileWord } from 'react-icons/fa';
 
 const DownloadLinks = () => (
   <div className="flex justify-center mt-8">
-    <Link href="/Ross_CV.pdf" passHref>
-      <div className="text-red-500 hover:text-gray-700 mr-4 flex items-center cursor-pointer">
+    <Link href="/Ross_CV.pdf" passHref legacyBehavior>
+      <a className="text-red-500 hover:text-gray-700 mr-4 flex items-center">
         <FaFilePdf className="mr-2" />
         Download PDF
-      </div>
+      </a>
     </Link>
 
-    <Link href="/Ross_CV.docx" passHref>
-      <div className="text-blue-500 hover:text-gray-700 flex items-center cursor-pointer">
+    <Link href="/Ross_CV.docx" passHref legacyBehavior>
+      <a className="text-blue-500 hover:text-gray-700 flex items-center">
         <FaFileWord className="mr-2" />
         Download Word
-      </div>
+      </a>
     </Link>
   </div>
 );
